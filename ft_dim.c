f30ce864e7e6e54c374e232c3e97ad99bafdc4be
Malo BOUCE <mabouce@e3r13p6.42.fr>
2018-11-23 14:47:31 +0100

    less leaks

--- ft_dim.c
+++ ft_dim.c
@@ -6,7 +6,7 @@
 /*   By: vifonne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 15:28:43 by vifonne           #+#    #+#             */
-/*   Updated: 2018/11/23 14:41:15 by mabouce          ###   ########.fr       */
+/*   Updated: 2018/11/23 14:46:40 by mabouce          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		ft_check_dim(char *str, int i, int n, int x)
 			if (tab[i][j] == '#')
 				x++;
 		if (j != 4)
-			ft_error();
+			ft_error_tab_str(tab, str);
 		if (n % 4 == 0 && i != 0 && x == 4)
 		{
 			x = 0;
@@ -43,20 +43,22 @@ int		ft_check_dim(char *str, int i, int n, int x)
 int		ft_check_dim2(char *str)
 {
 	int	i;
+	int j;
 
+	j = 0;
 	i = 0;
-	while (*str)
+	while (str[j])
 	{
-		if (*str == '\n' && *(str + 1) == '\n')
+		if (str[j] == '\n' && str[j + 1] == '\n')
 		{
 			if (i != 19)
-				ft_error();
+				ft_error_str(str);
 			else
 				i = -1;
 		}
 		else
 			i++;
-		str++;
+		j++;
 	}
 	return (0);
 }
